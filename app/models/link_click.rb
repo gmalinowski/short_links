4e0ab30a89326827c9e703ff9214ef594d3eee28
Greg M <git.15m27@passmail.net>
2025-09-19 11:40:36 +0200

    Add realtime link views counter

--- app/models/link_click.rb
+++ app/models/link_click.rb
@@ -1,4 +1,12 @@
-
 class LinkClick < ApplicationRecord
   belongs_to :short_link
+
+  after_create_commit do
+    broadcast_replace_to "short_links",
+                         target: "link_clicks_count_#{short_link.id}",
+                         partial: "short_links/link_count",
+                         locals: {
+                           short_link: short_link
+                         }
+  end
 end
